3f760cbc8ab81407fccd30ad131b86eb47dbef50
Jacob Horner <horner.jacob11@gmail.com>
2021-05-06 18:14:38 -0500

    Update barscript.js

--- assets/js/barscript.js
+++ assets/js/barscript.js
@@ -1,19 +1,17 @@
-var username = "";
 
-print username;
-if (username = "") {
+function startPage() {
+  username = "";
   var element = document.getElementById("Heading1");
   element.innerHTML = "Welcome to Jacob's Bar!";
   element = document.getElementById("Question");
   element.innerHTML = "What should we call you?";
-}
-else {
-  var element = document.getElementById("Heading1");
-  element.innerHTML = "Welcome " + username + "!";
-  element = document.getElementById("Question");
-  element.innerHTML = "";
+  print username;
 }
 
 function getInputValue(){
   username = document.getElementById("input").value;
+  var element = document.getElementById("Heading1");
+  element.innerHTML = "Welcome " + username + "!";
+  element = document.getElementById("Question");
+  element.innerHTML = "";
 }
