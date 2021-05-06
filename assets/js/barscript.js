2afb843bdd8cb131dd40acd840030e1ff3b8e1a9
Jacob Horner <horner.jacob11@gmail.com>
2021-05-06 17:12:43 -0500

    Update barscript.js

--- assets/js/barscript.js
+++ assets/js/barscript.js
@@ -1,15 +1,17 @@
 var username = "";
 
-if (username = "")
+if (username = "") {
   var element = document.getElementById("Heading1");
   element.innerHTML = "Welcome to Jacob's Bar!";
   element = document.getElementById("Question");
   element.innerHTML = "What should we call you?";
-else
+}
+else {
   var element = document.getElementById("Heading1");
   element.innerHTML = "Welcome " + username + "!";
   element = document.getElementById("Question");
   element.innerHTML = "";
+}
 
 function getInputValue(){
   username = document.getElementById("input").value;
