63a0b8dc25a648a3a96c5ffe279c51c5d5a89407
Jacob Horner <horner.jacob11@gmail.com>
2021-05-06 21:16:17 -0500

    Update barscript.js

--- assets/js/barscript.js
+++ assets/js/barscript.js
@@ -1,20 +1,65 @@
-
 function startPage() {
-  console.log("In startPage function");
-  username = "";
-  var element = document.getElementById("Heading1");
-  element.innerHTML = "Welcome to Jacob's Bar!";
-  element = document.getElementById("Question");
-  element.innerHTML = "What should we call you?";
-  console.log(username);
+  heading = document.getElementById("Heading1");
+  heading.innerHTML = "Welcome to Jacob's Bar!";
+  question = document.getElementById("Question");
+  question.innerHTML = "What should we call you?";
+  drinkButton = document.getElementById("DrinkButton");
+  drinkButton.style.visibility = "hidden";
 }
 
-function getInputValue(){
-  console.log("In getInputValue function");
-  username = document.getElementById("input").value;
-  var element = document.getElementById("Heading1");
-  element.innerHTML = "Welcome " + username + "!";
-  element = document.getElementById("Question");
-  element.innerHTML = "";
-  console.log(username);
+function getInputValue() {
+  username = document.getElementById("Input").value;
+  changeDisplay();
+}
+
+function changeDisplay() {
+  heading.innerHTML = "Welcome " + username + "!";
+  question.style.visibility = "hidden";
+  textField = document.getElementById("Input");
+  textField.style.visibility = "hidden";
+  button = document.getElementById("Button")
+  button.style.visibility = "hidden";
+  wantADrink();
+}
+
+function wantADrink() {
+  drinkButton.style.visibility = "visible";
+  question = document.getElementById("Question");
+  question.style.visibility = "visible";
+  question.innerHTML = "Would you like a drink?";
+  drinkButton.style.visibility = "visible";
+}
+
+function drinkRequested() {
+  question.style.visibility = "hidden";
+  drinkButton.style.visibility = "hidden";
+  heading.innerHTML = "You drink is on the way " + username + "!";
+  var drink = "Mojito";
+  writeDrinkOrder(username, drink);
+}
+
+function writeDrinkOrder(username, drink) {
+  var ordersListRef = firebase.database().ref('orders');
+  var newOrderRef = ordersListRef.push();
+  newOrderRef.set({
+    username: username,
+    drink: drink
+  });
+  displayQueue();
+}
+
+function displayQueue() {
+  readData();
+}
+
+function readData() {
+  var ref = firebase.database().ref("orders");
+
+  ref.on("value", function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      var childData = childSnapshot.val();
+      var drink = childData.drink;
+      heading.innerHTML = "Somebody get " + childData.username + " a " + childData.drink + "!"
+    });
+  });
 }
