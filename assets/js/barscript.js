3e5827bfb868759b4d340398610bb7379fa9ff29
Jacob Horner <horner.jacob11@gmail.com>
2021-05-08 13:27:54 -0500

    Update barscript.js

--- assets/js/barscript.js
+++ assets/js/barscript.js
@@ -5,6 +5,8 @@ function startPage() {
   question.innerHTML = "What should we call you?";
   drinkButton = document.getElementById("DrinkButton");
   drinkButton.style.visibility = "hidden";
+  dbRef = firebase.database().ref("orders");
+  tableFlag = "N"
 }
 
 function getInputValue() {
@@ -45,32 +47,52 @@ function writeDrinkOrder(username, drink) {
     username: username,
     drink: drink
   });
-  displayQueue();
-}
-
-function displayQueue() {
   readData();
+  /*key = newOrderRef.getKey();
+  dbRef.child(key).limitToLast(1).on('child_added', (data) => {
+  	readData(data);
+  });*/
 }
 
 function readData() {
-  var ref = firebase.database().ref("orders");
-  var table = document.createElement('table');
+  table = document.createElement('table');
+  /*dbRef.on('child_added', (data) => {
+    readData(data);
+  });*/
+  if (tableFlag = "N") {
+    dbRef.on("value", function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        childSnapshot.forEach(function(childSnapshot) {
+          console.log(childSnapshot.val());
+          var drink = childSnapshot.val().drink;
+          var user = childSnapshot.val().username;
+          var tr = document.createElement('tr');
 
-  ref.on("value", function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
-    	var childData = childSnapshot.val();
-    	var tr = document.createElement('tr');   
+          var td1 = document.createElement('td');
 
+          var text1 = document.createTextNode("Somebody get " + user + " a " + drink + "!");
+
+          td1.appendChild(text1);
+          tr.appendChild(td1);
+
+          table.appendChild(tr);
+          tableFlag = "Y";
+        });
+      });
+    });
+  } else {
+    dbRef.limitToLast(1).on("child_added", function(snap) {
+      console.log("tableFlag = Y");
+      var drink = snap.val().drink;
+      var user = snap.val().username;
+      var tr = document.createElement('tr');
       var td1 = document.createElement('td');
-
-      var text1 = document.createTextNode("Somebody get " + childData.username + " a " + childData.drink + "!");
-
+      var text1 = document.createTextNode("Somebody get " + user + " a " + drink + "!");
       td1.appendChild(text1);
       tr.appendChild(td1);
 
       table.appendChild(tr);
-      var drink = childData.drink;
     });
-    document.body.appendChild(table);
-  });
+  }
+  document.body.appendChild(table);
 }
